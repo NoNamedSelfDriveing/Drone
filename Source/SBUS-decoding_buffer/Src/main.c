8ef09eb4b39c13bb8188487b79d3353fb5cb0e81
Soomin Lee <magmatart@gmail.com>
2017-11-12 22:55:21 +0900

    seperate sbus header

--- Source/SBUS-decoding_buffer/Src/main.c
+++ Source/SBUS-decoding_buffer/Src/main.c
@@ -46,65 +46,13 @@
 
 /* USER CODE BEGIN Includes */
 #include <string.h>
+#include "sbus.h"
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-
-#define COUNTOF(__BUFFER__) (sizeof(__BUFFER__)/sizeof(*(__BUFFER__)))
-#define BUFFERSIZE(buffer) (COUNTOF(buffer))
-#define IS_STACKING_BUFFER_FULL(sbus) ((sbus).uart_rx_stacking_idx>=25)
-
-#define SBUS_DATA_SIZE 23
-#define UART_DATA_SIZE 25
-#define STACKING_SIZE 250
-#define ROW_SIZE 10
-#define START_BYTE 0x0f
-#define END_BYTE 0x04
-
-typedef struct _SBUS_PWM
-{
-  float pwm;
-  float min_pwm;
-  float max_pwm;
-  float min_duty;
-  float max_duty;
-  
-  TIM_HandleTypeDef* htim;
-  uint32_t channel;
-  
-  float F_dutyCycle;
-  uint16_t DutyCycle; 
-}SBUS_pwm;
-
-typedef struct _SBUS
-{
-  uint8_t         uart_rx_receive_buff[11];
-  uint8_t         uart_rx_stacking_buff[STACKING_SIZE];
-  uint8_t		  uart_rx_decoding_buff[STACKING_SIZE];
-  uint8_t		  remained_after_decoding[STACKING_SIZE];
-  uint8_t         uart_rx_buff[ROW_SIZE][UART_DATA_SIZE];
-
-  uint16_t        data_buff[ROW_SIZE][18];
-  
-  uint16_t		  uart_rx_data_idx;
-  uint16_t		  uart_rx_stacking_idx;
-  uint16_t        uart_rx_row_idx;
-  uint16_t        sb_index_saver;
-  
-  uint16_t        idx;
-  
-  volatile        uint8_t rx_flag;
-  volatile        uint8_t start_flag;
-
-  SBUS_pwm sbus_pwm;
-}SBUS;
-
-// SBUS
-SBUS_pwm sbus_pwm;
-SBUS sbus;
 
 /* USER CODE END PV */
 
@@ -113,17 +61,6 @@ void SystemClock_Config(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
-void init_sbus_pwm(TIM_HandleTypeDef*, uint32_t);
-void init_sbus();
-
-// make next decodeable buffer
-void make_next_decodeable_buffer();
-  
-// check sbus data packet (start byte, end byte) when sbus data stacked 25 bytes
-void check_sbus_data_packet();
-
-// decode sbus data
-void decode_sbus_data();
 
 /* USER CODE END PFP */
 
@@ -178,7 +115,7 @@ int main(void)
   {
 	if( IS_STACKING_BUFFER_FULL(sbus) )
     {
-	  make_next_decodeable_buffer();
+	    make_next_decodeable_buffer();
       check_sbus_data_packet();
       decode_sbus_data();
     }
@@ -247,122 +184,6 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-
-void init_sbus_pwm(TIM_HandleTypeDef * htim, uint32_t channel){
-    sbus_pwm.min_duty = 4598;
-    sbus_pwm.max_duty = 8126;
-    sbus_pwm.max_pwm = 1696;
-    sbus_pwm.min_pwm = 352;
-    sbus_pwm.pwm = 0;
-
-    sbus_pwm.htim = htim;
-    sbus_pwm.channel = channel;
-}
-
-void init_sbus(){
-    sbus.sbus_pwm = sbus_pwm;
-
-    sbus.uart_rx_data_idx = 0;
-    sbus.uart_rx_stacking_idx = 0;
-    sbus.sb_index_saver = 0;
-    sbus.rx_flag = 0;
-    sbus.start_flag = 0;
-}
-
-void make_next_decodeable_buffer()
-{  
-  sbus.uart_rx_stacking_idx = 0;
-  memcpy(sbus.uart_rx_decoding_buff, sbus.remained_after_decoding, sizeof(uint8_t) * sbus.sb_index_saver);
-  memcpy(sbus.uart_rx_decoding_buff + sbus.sb_index_saver, sbus.uart_rx_stacking_buff, sizeof(uint8_t) * (STACKING_SIZE - sbus.sb_index_saver) );
-}
-
-void check_sbus_data_packet()
-{ 
-  uint8_t sb_index = 0;        // Start byte index
-  uint8_t is_index_over = 0;
-  uint8_t uart_rx_row_index = 0;
-  
-  while(sb_index < STACKING_SIZE){
-
-    // Increase index until find Start byte
-    if(sbus.uart_rx_decoding_buff[sb_index] != START_BYTE && sbus.start_flag == 0)
-    {
-	  sbus.start_flag = 0;
-	  sb_index++;
-	  continue;
-    }
-
-    if(sbus.uart_rx_decoding_buff[sb_index] == START_BYTE && sbus.start_flag == 0)
-    {
-      // Search to full packet
-      while( !(sbus.uart_rx_decoding_buff[sb_index] == START_BYTE && sbus.uart_rx_decoding_buff[sb_index + (UART_DATA_SIZE-1)]%16 == END_BYTE) )
-      {
-        sb_index++;
-		if(sb_index + (UART_DATA_SIZE-1) > STACKING_SIZE-1){
-		  is_index_over = 1;
-		  break;
-		}
-      }
-
-      if(is_index_over)
-        break;
-      
-      sbus.start_flag = 1;
-    }
-    
-    // If start flag is on
-    if(sbus.start_flag)
-    {
-	  memcpy((sbus.uart_rx_buff[uart_rx_row_index]), (sbus.uart_rx_decoding_buff+sb_index), sizeof(uint8_t)*UART_DATA_SIZE);
-	  uart_rx_row_index++;
-
-      // location of next packet's start byte
-      sb_index++;
-	  sbus.start_flag = 0;
-    }
-  }
-
-  memcpy(sbus.remained_after_decoding, sbus.uart_rx_decoding_buff + sb_index, sizeof(uint8_t) * (STACKING_SIZE - sb_index)); 
-  
-  memset(sbus.uart_rx_decoding_buff, 0, sizeof(uint8_t)*STACKING_SIZE);
-  
-  sbus.uart_rx_row_idx = uart_rx_row_index;
-  sbus.sb_index_saver = (STACKING_SIZE - sb_index);  // 다음 버퍼에서 뒤에 이어붙이기 시작할 인덱스
-}
-
-void decode_sbus_data()
-{ 
-  uint8_t row_index = 0;
-  for(row_index = 0 ; row_index < sbus.uart_rx_row_idx ; row_index++)
-  {
-    sbus.data_buff[row_index][0] = sbus.uart_rx_buff[row_index][1] + (uint16_t)((sbus.uart_rx_buff[row_index][2]&0x07)<<8);
-    sbus.data_buff[row_index][1] = (uint16_t)((sbus.uart_rx_buff[row_index][2]&0xf8)>>3) + (uint16_t)((sbus.uart_rx_buff[row_index][3]&0x3f)<<5);
-    sbus.data_buff[row_index][2] = (uint16_t)((sbus.uart_rx_buff[row_index][3]&0xc0)>>6) + (uint16_t)(sbus.uart_rx_buff[row_index][4]<<2) + (uint16_t)((sbus.uart_rx_buff[row_index][5]&0x01)<<10);
-    sbus.data_buff[row_index][3] = (uint16_t)((sbus.uart_rx_buff[row_index][5]&0xfe)>>1) + (uint16_t)((sbus.uart_rx_buff[row_index][6]&0x0f)<<7);
-    sbus.data_buff[row_index][4] = (uint16_t)((sbus.uart_rx_buff[row_index][6]&0xf0)>>4) + (uint16_t)((sbus.uart_rx_buff[row_index][7]&0x7f)<<4);
-    sbus.data_buff[row_index][5] = (uint16_t)((sbus.uart_rx_buff[row_index][7]&0x80)>>7) + (uint16_t)(sbus.uart_rx_buff[row_index][8]<<1) + (uint16_t)((sbus.uart_rx_buff[row_index][9]&0x03)<<9);
-    sbus.data_buff[row_index][6] = (uint16_t)((sbus.uart_rx_buff[row_index][9]&0xfc)>>2) + (uint16_t)((sbus.uart_rx_buff[row_index][10]&0x1f)<<6);
-  	printf("%.4d\r\n", sbus.data_buff[row_index][2]);	// Throttle of Altitude (CH.2) 
-  }
-}
-
-/*
-
-void sbus_pwm_make(uint16_t F_value){
-   //F_dutyCycle : (((value - 352)/ 1344) * 100 * 0.05) + 7
-   uint16_t pulse;
-   
-   sbus_pwm.F_dutyCycle = ((F_value - 352) * 5.5 / 1344) +7;
-   pulse = (((64499 + 1) * sbus_pwm.F_dutyCycle) / 100) - 1;
-   
-   sbus_pwm.DutyCycle = pulse;
-   __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, sbus_pwm.DutyCycle);
-    
-   htim1.Instance -> CCR1 = sbus_pwm.DutyCycle;
-   
-   printf("%.4d %.4d %.3d \r\n", F_value, sbus_pwm.DutyCycle, sbus_pwm.F_dutyCycle);
-}
-*/
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 { 
